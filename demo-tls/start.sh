68800c255ead21b4cafd86c8a44f1baf5a5b04b0
danijelradakovic <danijelradakovic1996@gmail.com>
2020-04-07 17:22:11 +0200

    Created docker compose template

--- demo-tls/start.sh
+++ demo-tls/start.sh
@@ -1,19 +1,85 @@
 #! /bin/bash
 
-BASE_DIR=${1:-$(pwd)}
+BASE_DIR=$(pwd)
 TLS_DIR=${BASE_DIR}/tls
+DOCKER_COMPOSE_TEMPLATE=${BASE_DIR}/docker-compose-template.yml
+DOCKER_COMPOSE=${BASE_DIR}/docker-compose.yml
 
-# Clean up previous generated certificates
-rm -rf ${TLS_DIR}/ca ${TLS_DIR}/certs ${TLS_DIR}/crl
+KEY_SIZE=${2:-4096}
 
-# Generate certificates
-docker run \
+SERVERS_NAME=${3:-servers}
+SERVERS_ALIAS=${4:-servers}
+SERVERS_KEYSTORE_PASSWORD=${5:-password}
+
+REPORT_NAME=${6:-report}
+REPORT_ALIAS=${7:-report}
+REPORT_KEYSTORE_PASSWORD=${8:-password}
+
+CLIENT_NAME=${9:-client}
+CLIENT_ALIAS=${10:-client}
+CLIENT_KEYSTORE_PASSWORD=${11:-password}
+
+LOGAN_NAME=${12:-logan}
+LOGAN_ALIAS=${13:-logan}
+LOGAN_KEYSTORE_PASSWORD=${14:-password}
+
+RMQ_NAME=${15:-rabbitmq}
+
+function generateDockerCompose() {
+  # sed on MacOSX does not support -i flag with a null extension. We will use
+  # 't' for our back-up's extension and delete it at the end of the function
+  ARCH=$(uname -s | grep Darwin)
+  if [[ "$ARCH" == "Darwin" ]]; then
+    OPTS="-it"
+  else
+    OPTS="-i"
+  fi
+
+  # Copy the template to the file that will be modified to add certificates
+  cp "${DOCKER_COMPOSE_TEMPLATE}" "${DOCKER_COMPOSE}" || exit
+
+  # The next steps will replace the template's contents with the
+  # actual values of the certificates and private key files
+  sed ${OPTS} \
+      -e "s/SERVERS_NAME/${SERVERS_NAME}/g" \
+      -e "s/SERVERS_ALIAS/${SERVERS_ALIAS}/g" \
+      -e "s/SERVERS_KEYSTORE_PASSWORD/${SERVERS_KEYSTORE_PASSWORD}/g" \
+      -e "s/REPORT_NAME/${REPORT_NAME}/g" \
+      -e "s/REPORT_ALIAS/${REPORT_ALIAS}/g" \
+      -e "s/REPORT_KEYSTORE_PASSWORD/${REPORT_KEYSTORE_PASSWORD}/g" \
+      -e "s/LOGAN_NAME/${LOGAN_NAME}/g" \
+      -e "s/LOGAN_ALIAS/${LOGAN_ALIAS}/g" \
+      -e "s/LOGAN_KEYSTORE_PASSWORD/${LOGAN_KEYSTORE_PASSWORD}/g" \
+      -e "s/RMQ_NAME/${RMQ_NAME}/g" \
+      "${DOCKER_COMPOSE}"
+
+  if [[ "$ARCH" == "Darwin" ]]; then
+    rm "${DOCKER_COMPOSE}t"
+  fi
+}
+
+function generateCertificates() {
+  # Clean up previous generated certificates
+  rm -rf "${TLS_DIR}"/ca "${TLS_DIR}"/certs "${TLS_DIR}"/crl
+
+  docker run \
        -it \
        --rm \
        --entrypoint sh \
        --mount "type=bind,source=${TLS_DIR},destination=/export" \
        --user "$(id -u):$(id -g)" \
        danijelradakovic/openssl-keytool \
-       gen.sh
+       gen.sh "${KEY_SIZE}" \
+       "${SERVERS_NAME}" "${SERVERS_ALIAS}" "${SERVERS_KEYSTORE_PASSWORD}" \
+       "${REPORT_NAME}"  "${REPORT_ALIAS}"  "${REPORT_KEYSTORE_PASSWORD}"  \
+       "${CLIENT_NAME}"  "${CLIENT_ALIAS}"  "${CLIENT_KEYSTORE_PASSWORD}"  \
+       "${LOGAN_NAME}"   "${LOGAN_ALIAS}"   "${LOGAN_KEYSTORE_PASSWORD}"   \
+       "${RMQ_NAME}"
+}
 
+if [ "${1}" == '-g' ]; then
+  generateCertificates
+  generateDockerCompose
+fi
 
+docker-compose up --build
--- demo-tls/tls/gen.sh
+++ demo-tls/tls/gen.sh
@@ -1,12 +1,25 @@
 #! /bin/sh
 
-SERVERS_NAME=${1:-servers}
-SERVERS_ALIAS=${2:-servers}
-SERVERS_KEYSTORE_PASSWORD=${3:-password}
+KEY_SIZE=${1:-4096}
 
-CLIENT_NAME=${4:-client}
-CLIENT_ALIAS=${5:-client}
-CLIENT_KEYSTORE_PASSWORD=${6:-password}
+SERVERS_NAME=${2:-servers}
+SERVERS_ALIAS=${3:-servers}
+SERVERS_KEYSTORE_PASSWORD=${4:-password}
+
+REPORT_NAME=${5:-report}
+REPORT_ALIAS=${6:-report}
+REPORT_KEYSTORE_PASSWORD=${7:-password}
+
+CLIENT_NAME=${8:-client}
+CLIENT_ALIAS=${9:-client}
+CLIENT_KEYSTORE_PASSWORD=${10:-password}
+
+LOGAN_NAME=${11:-logan}
+LOGAN_ALIAS=${12:-logan}
+LOGAN_KEYSTORE_PASSWORD=${13:-password}
+
+RMQ_NAME=${14:-rabbitmq}
+
 
 # Root CA
 mkdir -p ca/root-ca/private ca/root-ca/db crl certs
@@ -78,17 +91,18 @@ openssl ca \
 cat ca/tls-ca.crt ca/root-ca.crt > \
     ca/tls-ca-chain.pem
 
-mkdir -p certs/${SERVERS_NAME}/keystore
+mkdir -p "certs/${SERVERS_NAME}/keystore"
 
 # Create TLS server request
-SAN=DNS:green.no,DNS:www.green.no \
+SAN=DNS:green.no,DNS:www.green.no,DNS:localhost \
 openssl req \
     -new \
     -nodes \
+    -newkey "rsa:${KEY_SIZE}" \
     -config etc/server.conf \
     -out "certs/${SERVERS_NAME}/${SERVERS_NAME}.csr" \
     -keyout "certs/${SERVERS_NAME}/${SERVERS_NAME}.key" \
-    -subj "/C=NO/O=Green AS/OU=Green Certificate Authority/CN=localhost" || exit
+    -subj "/C=NO/O=Green AS/OU=Green Certificate Authority/CN=servers" || exit
 
 # Create TLS server certificate
 openssl ca \
@@ -102,30 +116,104 @@ openssl ca \
 # Export TLS server certificate chain to keystore
 openssl pkcs12 \
     -export \
-    -name ${SERVERS_ALIAS} \
+    -name "${SERVERS_ALIAS}" \
     -inkey "certs/${SERVERS_NAME}/${SERVERS_NAME}.key" \
     -in "certs/${SERVERS_NAME}/${SERVERS_NAME}.crt" \
     -certfile ca/tls-ca-chain.pem \
     -passout "pass:${SERVERS_KEYSTORE_PASSWORD}" \
     -out "certs/${SERVERS_NAME}/keystore/${SERVERS_NAME}.keystore.p12" || exit
 
-# Import Root CA and TLS CA certificate into server keystore 
-#keytool \
-#    -importcert \
-#    -noprompt \
-#    -alias "root" \
-#    -file ca/root-ca.crt \
-#    -storepass ${SERVERS_KEYSTORE_PASSWORD} \
-#    -storetype "PKCS12" \
-#    -keystore "certs/${SERVERS_NAME}/keystore/${SERVERS_NAME}.truststore.p12" &&
-keytool \
-    -importcert \
-    -noprompt \
-    -alias "tls" \
-    -file ca/tls-ca.crt \
-    -storepass ${SERVERS_KEYSTORE_PASSWORD} \
-    -storetype "PKCS12" \
-    -keystore "certs/${SERVERS_NAME}/keystore/${SERVERS_NAME}.truststore.p12" || exit
+
+mkdir -p "certs/${REPORT_NAME}/keystore"
+
+# Create TLS report request
+SAN=DNS:green.no,DNS:www.green.no,DNS:localhost \
+openssl req \
+    -new \
+    -nodes \
+    -newkey "rsa:${KEY_SIZE}" \
+    -config etc/server.conf \
+    -out "certs/${REPORT_NAME}/${REPORT_NAME}.csr" \
+    -keyout "certs/${REPORT_NAME}/${REPORT_NAME}.key" \
+    -subj "/C=NO/O=Green AS/OU=Green Certificate Authority/CN=report" || exit
+
+# Create TLS report certificate
+openssl ca \
+    -rand_serial \
+    -batch \
+    -config etc/tls-ca.conf \
+    -in "certs/${REPORT_NAME}/${REPORT_NAME}.csr" \
+    -out "certs/${REPORT_NAME}/${REPORT_NAME}.crt" \
+    -extensions server_ext || exit
+
+# Export TLS report certificate chain to keystore
+openssl pkcs12 \
+    -export \
+    -name "${REPORT_ALIAS}" \
+    -inkey "certs/${REPORT_NAME}/${REPORT_NAME}.key" \
+    -in "certs/${REPORT_NAME}/${REPORT_NAME}.crt" \
+    -certfile ca/tls-ca-chain.pem \
+    -passout "pass:${REPORT_KEYSTORE_PASSWORD}" \
+    -out "certs/${REPORT_NAME}/keystore/${REPORT_NAME}.keystore.p12" || exit
+
+
+mkdir -p "certs/${LOGAN_NAME}/keystore"
+
+# Create TLS logan request
+SAN=DNS:green.no,DNS:www.green.no,DNS:localhost \
+openssl req \
+    -new \
+    -nodes \
+    -newkey "rsa:${KEY_SIZE}" \
+    -config etc/server.conf \
+    -out "certs/${LOGAN_NAME}/${LOGAN_NAME}.csr" \
+    -keyout "certs/${LOGAN_NAME}/${LOGAN_NAME}.key" \
+    -subj "/C=NO/O=Green AS/OU=Green Certificate Authority/CN=logan" || exit
+
+# Create TLS logan certificate
+openssl ca \
+    -rand_serial \
+    -batch \
+    -config etc/tls-ca.conf \
+    -in "certs/${LOGAN_NAME}/${LOGAN_NAME}.csr" \
+    -out "certs/${LOGAN_NAME}/${LOGAN_NAME}.crt" \
+    -extensions server_ext || exit
+
+# Export TLS logan certificate chain to keystore
+openssl pkcs12 \
+    -export \
+    -name "${LOGAN_ALIAS}" \
+    -inkey "certs/${LOGAN_NAME}/${LOGAN_NAME}.key" \
+    -in "certs/${LOGAN_NAME}/${LOGAN_NAME}.crt" \
+    -certfile ca/tls-ca-chain.pem \
+    -passout "pass:${LOGAN_KEYSTORE_PASSWORD}" \
+    -out "certs/${LOGAN_NAME}/keystore/${LOGAN_NAME}.keystore.p12" || exit
+
+
+mkdir -p "certs/${RMQ_NAME}"
+
+# Create TLS rabbitmq request
+SAN=DNS:green.no,DNS:www.green.no,DNS:localhost \
+openssl req \
+    -new \
+    -nodes \
+    -newkey "rsa:${KEY_SIZE}" \
+    -config etc/server.conf \
+    -out "certs/${RMQ_NAME}/${RMQ_NAME}.csr" \
+    -keyout "certs/${RMQ_NAME}/${RMQ_NAME}.key" \
+    -subj "/C=NO/O=Green AS/OU=Green Certificate Authority/CN=rabbitmq" || exit
+
+# Create TLS rabbitmq certificate
+openssl ca \
+    -rand_serial \
+    -batch \
+    -config etc/tls-ca.conf \
+    -in "certs/${RMQ_NAME}/${RMQ_NAME}.csr" \
+    -out "certs/${RMQ_NAME}/${RMQ_NAME}.crt" \
+    -extensions server_ext || exit
+
+cp ca/tls-ca-chain.pem "certs/${RMQ_NAME}"
+
 
 mkdir -p "certs/${CLIENT_NAME}"
 
@@ -133,10 +221,11 @@ mkdir -p "certs/${CLIENT_NAME}"
 openssl req \
     -new \
     -nodes \
+    -newkey "rsa:${KEY_SIZE}" \
     -config etc/client.conf \
     -out "certs/${CLIENT_NAME}/${CLIENT_NAME}.csr" \
     -keyout "certs/${CLIENT_NAME}/${CLIENT_NAME}.key" \
-    -subj "/C=NO/O=Green AS/OU=Green Certificate Authority/CN=${CLIENT_NAME}" || exit
+    -subj "/C=NO/O=Green AS/OU=Green Certificate Authority/CN=Peter Peterson" || exit
     
 # Create TLS client certificate (for web browser)
 openssl ca \
@@ -148,21 +237,125 @@ openssl ca \
     -policy extern_pol \
     -extensions client_ext || exit
 
+# Export TLS client certificate chain to keystore
 openssl pkcs12 \
     -export \
-    -name ${CLIENT_ALIAS} \
+    -name "${CLIENT_ALIAS}" \
     -inkey "certs/${CLIENT_NAME}/${CLIENT_NAME}.key" \
     -in "certs/${CLIENT_NAME}/${CLIENT_NAME}.crt" \
     -certfile ca/tls-ca-chain.pem \
     -passout "pass:${CLIENT_KEYSTORE_PASSWORD}" \
     -out "certs/${CLIENT_NAME}/${CLIENT_NAME}.p12" || exit
 
-# Import client certificate into server keystore 
+
+# Import Root CA, TLS CA, report, rabbitmq and client certificate into server keystore
 keytool \
     -importcert \
     -noprompt \
-    -alias "client" \
+    -alias "root" \
+    -file ca/root-ca.crt \
+    -storepass "${SERVERS_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${SERVERS_NAME}/keystore/${SERVERS_NAME}.truststore.p12"  && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "tls" \
+    -file ca/tls-ca.crt \
+    -storepass "${SERVERS_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${SERVERS_NAME}/keystore/${SERVERS_NAME}.truststore.p12" && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "${REPORT_ALIAS}" \
+    -file "certs/${REPORT_NAME}/${REPORT_NAME}.crt" \
+    -storepass "${SERVERS_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${SERVERS_NAME}/keystore/${SERVERS_NAME}.truststore.p12" && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "${RMQ_NAME}" \
+    -file "certs/${RMQ_NAME}/${RMQ_NAME}.crt" \
+    -storepass "${SERVERS_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${SERVERS_NAME}/keystore/${SERVERS_NAME}.truststore.p12" && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "${CLIENT_ALIAS}" \
     -file "certs/${CLIENT_NAME}/${CLIENT_NAME}.crt" \
-    -storepass ${SERVERS_KEYSTORE_PASSWORD} \
+    -storepass "${SERVERS_KEYSTORE_PASSWORD}" \
     -storetype "PKCS12" \
     -keystore "certs/${SERVERS_NAME}/keystore/${SERVERS_NAME}.truststore.p12" || exit
+
+
+# Import Root CA, TLS CA, servers, rabbitmq and client certificate into report keystore
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "root" \
+    -file ca/root-ca.crt \
+    -storepass "${REPORT_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${REPORT_NAME}/keystore/${REPORT_NAME}.truststore.p12"  && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "tls" \
+    -file ca/tls-ca.crt \
+    -storepass "${REPORT_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${REPORT_NAME}/keystore/${REPORT_NAME}.truststore.p12" && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "${SERVERS_ALIAS}" \
+    -file "certs/${SERVERS_NAME}/${SERVERS_NAME}.crt" \
+    -storepass "${REPORT_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${REPORT_NAME}/keystore/${REPORT_NAME}.truststore.p12" && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "${RMQ_NAME}" \
+    -file "certs/${RMQ_NAME}/${RMQ_NAME}.crt" \
+    -storepass "${REPORT_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${REPORT_NAME}/keystore/${REPORT_NAME}.truststore.p12" && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "${CLIENT_ALIAS}" \
+    -file "certs/${CLIENT_NAME}/${CLIENT_NAME}.crt" \
+    -storepass "${REPORT_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${REPORT_NAME}/keystore/${REPORT_NAME}.truststore.p12" || exit
+
+
+# Import Root CA, TLS CA and rabbitmq certificate into logan keystore
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "root" \
+    -file ca/root-ca.crt \
+    -storepass "${LOGAN_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${LOGAN_NAME}/keystore/${LOGAN_NAME}.truststore.p12"  && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "tls" \
+    -file ca/tls-ca.crt \
+    -storepass "${LOGAN_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${LOGAN_NAME}/keystore/${LOGAN_NAME}.truststore.p12" && \
+keytool \
+    -importcert \
+    -noprompt \
+    -alias "${RMQ_NAME}" \
+    -file "certs/${RMQ_NAME}/${RMQ_NAME}.crt" \
+    -storepass "${LOGAN_KEYSTORE_PASSWORD}" \
+    -storetype "PKCS12" \
+    -keystore "certs/${LOGAN_NAME}/keystore/${LOGAN_NAME}.truststore.p12" || exit
